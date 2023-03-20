71d2ce0821afa920d1f23475aabde33a78f85f8b
luis-vanin <vanin@copybase.com.br>
2023-03-20 11:34:52 -0300

    MPPJ-creating useValidation hook

--- src/composables/form/useValidation.ts
+++ src/composables/form/useValidation.ts
@@ -1,18 +1,25 @@
-interface Rule {
-    required?: boolean;
-    minLength?: number;
-    maxLength?: number;
-    pattern?: RegExp;
-    validate?: (value: any) => boolean;
-    message?: string;
+import { ValidationError } from "./../../helpers/config/enums";
+
+interface ZodGeneric<T> {
+    parse: () => T;
 }
+
+type Rule<T = string> =
+    | {
+          validator: ZodGeneric<T>;
+          message: string;
+      }
+    | {
+          validation: (value: T) => boolean;
+          message: string;
+      };
 
 type Rules<T> = Record<keyof T, Rule>;
 
 const useValidation = <T>(input: Record<string, any>, rules: Rules<T>) => {
     const valid = ref(false);
-    const errors = ref<Record<keyof T, string | undefined>>(
-        {} as Record<keyof T, string>
+    const errors = ref<Record<keyof T, ValidationError>>(
+        {} as Record<keyof T, ValidationError>
     );
     // function that retiurn a Record<keyof T, string | undefined> with the error messages in each field
 
